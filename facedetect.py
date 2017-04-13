683f18d47219e988ebdcda3c6b10ad0fda6c2490
Daniel Felix-Kim <daniel.felixkim@gmail.com>
2017-04-12 22:24:15 -0500

    Added wrapper function for facedetect and added it to tornadoserver.py.

--- facedetect.py
+++ facedetect.py
@@ -1,29 +1,26 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('C:\Users\danie\Downloads\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('C:\Users\danie\Downloads\opencv\sources\data\haarcascades\haarcascade_eye.xml')
+def facedetect(image):
+	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+	eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img1 = cv2.imread('dfk.jpg')
-img2 = cv2.imread('harambe.jpg')
-gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+	img1 = cv2.imread(image)
+	gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray1, 1.3, 5)
 
-faces = face_cascade.detectMultiScale(gray1, 1.3, 5)
-face2 = face_cascade.detectMultiScale(gray2, 1.3, 5)
+	print faces
+	for (x,y,w,h) in faces:
+	    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+	    centerFrame = x+w/2,y+h/2
+	    cv2.rectangle(img,(centerFrame[0],centerFrame[1]),(centerFrame[0],centerFrame[1]),(0,0,255),2)
+	    roi_gray = gray[y:y+h, x:x+w]
+	    roi_color = img[y:y+h, x:x+w]
+	    eyes = eye_cascade.detectMultiScale(roi_gray)
+	    for (ex,ey,ew,eh) in eyes:
+	        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-faces = np.concatenate((faces, face2), axis=0)
-print faces
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    centerFrame = x+w/2,y+h/2
-    cv2.rectangle(img,(centerFrame[0],centerFrame[1]),(centerFrame[0],centerFrame[1]),(0,0,255),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+	cv2.imwrite('image.png',img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	return centerFrame
--- tornadoserver.py
+++ tornadoserver.py
@@ -1,10 +1,15 @@
 import tornado.ioloop
 import tornado.web
+import facedetect as fd
 
 class MainHandler(tornado.web.RequestHandler):
 	def post(self):
-		self.write(self.get_argument('data', ''))
+		image = self.get_argument('image', '')
+		center_face = fd.facedetect(image)
+		self.write(center_face)
 	get = post
+	
+
 
 def make_app():
     return tornado.web.Application([
