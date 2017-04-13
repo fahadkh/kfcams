afe37f216b1194f412b5bbd50ba21eb469e12bef
Fahad <fahadkhan2017@u.northwestern.edu>
2017-04-13 20:05:07 +0000

    cleaning up files

--- facedetect2.py
+++ facedetect2.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('classifiers/haarcascade_eye.xml')
 	
 #	print image
 #	fi = cStringIO.StringIO(image)
