b1312fd4afcfa27429636d580aa291a1476493b9
Fahad <fahadkhan2017@u.northwestern.edu>
2017-04-20 20:54:24 +0000

    add functions for streaming images over socket

--- tornadoserver.py
+++ tornadoserver.py
@@ -1,16 +1,39 @@
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
-from urlparse import urlparse
+import time
 import facedetect as fd
+from urlparse import urlparse
 
 clients = []
+
+
+class CamSocketHandler(tornado.websocket.WebSocketHandler):
+	def check_origin(self, origin):
+		parsed_origin = urlparse(origin)
+		print parsed_origin
+		with open("socket_access.log", "a") as f:
+			f.write(time.strftime("%c") + ": " + str(parsed_origin) + "\n")
+		return True
+
+	def open(self):
+		print("CamSocket opened")
+
+	def on_message(self, message):
+		print "received image: " + message
+		center_face=fd.facedetect(message)
+		print(center_face)
+		self.write(center_face)
+		update_clients()
+
+	def on_close(self):
+		print("CamSocket closed")
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
 	def check_origin(self, origin):
 		parsed_origin = urlparse(origin)
 		return parsed_origin.netloc.endswith("ec2-13-58-118-151.us-east-2.compute.amazonaws.com") or parsed_origin.netloc.endswith("13.58.118.151")
-
+	
 	def open(self):
 		print("WebSocket opened")
 		clients.append(self)
@@ -35,13 +58,13 @@ class MainHandler(tornado.web.RequestHandler):
 
 def update_clients():
 	for client in clients:
-		client.write_message("hi")
-		print("sent message to client")
+		client.write_message(".")
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-		(r"/socket", WebSocketHandler)
+		(r"/socket", WebSocketHandler),
+		(r"/camsocket", CamSocketHandler)
     ])
 
 if __name__ == "__main__":
